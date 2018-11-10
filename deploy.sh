79447dd41f1bd8a5abcb6e26b743231b85632c4f
brnicot <7538606+brnicot@users.noreply.github.com>
2018-11-10 16:42:40 +0100

    création du script de déploiement

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# Rappatriement depuis git de la branch
+cd /var/www/html
+git checkout
+
+# Installation des dependencies
+composer install
+
+# Maj de la BD
+php bin/console doctrine:schema:update --force
+
+# Suppression des cache
+php bin/console cache:clear --env=dev
+php bin/console cache:clear --env=prod
+
+# Reassignation des droits
+chown -hR www-data:www-data /var/www/html
