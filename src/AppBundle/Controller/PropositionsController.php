dd2300e9da3f0f730bc3e651496666a3c569a4d4
Mathieu <mathieu@math98.fr>
2018-11-28 23:05:52 +0100

    Filtrages sur la liste des propositions

--- src/AppBundle/Controller/PropositionsController.php
+++ src/AppBundle/Controller/PropositionsController.php
@@ -4,9 +4,12 @@ namespace AppBundle\Controller;
 
 use AppBundle\Entity\Classes;
 use AppBundle\Entity\Domaineactivite;
+use AppBundle\Entity\Entreprises;
 use AppBundle\Entity\Etat;
 use AppBundle\Entity\Propositions;
+use AppBundle\Entity\Technologies;
 use AppBundle\Form\PropositionsType;
+use Doctrine\ORM\EntityRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\File\Exception\FileException;
@@ -153,20 +156,60 @@ class PropositionsController extends Controller
     /**
      * @Route("/propositions", name="afficherProposition")
      */
-    public function showProposition()
+    public function showProposition(Request $request)
     {
+        /** @var EntityRepository $repository */
         $repository = $this->getDoctrine()
             ->getRepository(Propositions::class);
 
+        /** @var Domaineactivite[] $domainesAct */
+        $domainesAct = $this->getDoctrine()->getRepository(Domaineactivite::class)->findAll();
+
+        /** @var Classes[] $classes */
+        $classes = $this->getDoctrine()->getRepository(Classes::class)->findAll();
+
+        /** @var Technologies[] $technos */
+        $technos = $this->getDoctrine()->getRepository(Technologies::class)->findAll();
+
+        $checkedClasses = $request->get('classes');
+        $checkedDomaines = $request->get('domaines');
+        $checkedTechnos = $request->get('technos');
+
         $query = $repository->createQueryBuilder('p')
-            ->where('p.codeetat = 2')
-            ->orderBy('p.dateajout', 'DESC')
-            ->getQuery();
-        $domaineAct = $this->getDoctrine()->getRepository(Domaineactivite::class)->findAll();
+                            ->where('p.codeetat = 2');
 
-        $propositions = $query->getResult();
+        if (!is_null($checkedClasses)) {
+            $query->innerJoin('p.codeclasse','c')
+                ->andWhere('c.codeclasse IN (:classes)')
+                ->setParameter('classes',$checkedClasses);
+        }
 
-        return $this->render('propositions/propositionsShow.html.twig',['propositions' => $propositions, 'domaineActivites'=>$domaineAct]);
+        if (!is_null($checkedDomaines)) {
+            $query->join('p.codeentreprise', 'e', 'WITH', 'p.codeentreprise=e.codeentreprise')
+                ->innerJoin('e.codedomaine','d')
+                ->andWhere('d.codedomaine IN (:domaines)')
+                ->setParameter('domaines', $checkedDomaines);
+        }
+
+        if (!is_null($checkedTechnos)) {
+            $query->innerJoin('p.codetechnologie','t')
+                ->andWhere('t.codetechnologie IN (:technologies)')
+                ->setParameter('technologies',$checkedTechnos);
+        }
+
+        $propositions = $query->orderBy('p.dateajout', 'DESC')
+            ->getQuery()
+            ->getResult();
+
+        return $this->render('propositions/propositionsShow.html.twig',[
+            'propositions' => $propositions,
+            'domaineActivites'=> $domainesAct,
+            'checkedDomaines' => $checkedDomaines,
+            'technologies' => $technos,
+            'checkedTechnos' => $checkedTechnos,
+            'classes' => $classes,
+            'checkedClasses' => $checkedClasses
+        ]);
     }
 
     /**
