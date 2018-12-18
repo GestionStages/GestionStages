0fb7ab349f00132f1e32994826a9041e41959ae2
Mathieu <mathieu@math98.fr>
2018-12-18 20:36:43 +0100

    Ajout de Carbon, ajout du bundle HTML2PDF dans le kernel, modif de la config du bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new Ensepar\Html2pdfBundle\EnseparHtml2pdfBundle(),
             new AppBundle\AppBundle()
         ];
 
@@ -23,7 +24,6 @@ class AppKernel extends Kernel
             $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
-            $bundles[] = new Ensepar\Html2pdfBundle\EnseparHtml2pdfBundle();
 
             if ('dev' === $this->getEnvironment()) {
                 $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
