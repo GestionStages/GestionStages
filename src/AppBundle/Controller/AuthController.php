43f53c7ebb2cbac93cac459a7044c9b8e9fc6649
Mathieu <mathieu@math98.fr>
2019-01-06 02:04:22 +0100

    Corrections/Améliorations sur le AuthController

--- src/AppBundle/Controller/AuthController.php
+++ src/AppBundle/Controller/AuthController.php
@@ -4,20 +4,18 @@ namespace AppBundle\Controller;
 
 use AppBundle\Entity\Etudiant;
 use AppBundle\Entity\Professeur;
-use AppBundle\Entity\RolesProf;
 use AppBundle\Form\EtudiantType;
 use AppBundle\Form\ProfesseurType;
 use AppBundle\Repository\EtudiantRepository;
 use Doctrine\Common\Persistence\ObjectManager;
-use Doctrine\Common\Persistence\ObjectRepository;
-use Doctrine\ORM\EntityRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Http\Event\InteractiveLoginEvent;
 
 class AuthController extends Controller
@@ -29,7 +27,7 @@ class AuthController extends Controller
      * @param UserPasswordEncoderInterface $encoder
      * @return Response
      */
-    public function inscriptionEtu(Request $request, ObjectManager $em, UserPasswordEncoderInterface $encoder, EtudiantRepository $repository) {
+    public function inscriptionEtu(Request $request, ObjectManager $em, UserPasswordEncoderInterface $encoder) {
         $etudiant = new Etudiant();
         $form = $this->createForm(EtudiantType::class, $etudiant, ['validation_groups' => ['inscription', 'Default']]);
         $form->handleRequest($request);
@@ -37,51 +35,20 @@ class AuthController extends Controller
         if ($form->isSubmitted() && $form->isValid()) {
             $username = $etudiant->getUserEtudiant();
 
-            $dbResult = $repository->findOneByUserEtudiant($username);
+            $dbResult = $this->getDoctrine()->getRepository(Etudiant::class)->findOneByUserEtudiant($username);
             if (!is_null($dbResult)) {
                 $this->get('session')->getFlashBag()->add('error', "Cet utilisateur existe déjà, essayez de vous connecter");
                 return $this->redirectToRoute('connectEtu');
             }
 
-            $group = $etudiant->getCodeclasse()->getCodegroupeldap()->getLdapSection();
-
-            //Initialisation de la connexion au serveur LDAP
-            $conn  = ldap_connect($this->getParameter('ldap_host'), $this->getParameter('ldap_port'));
-            ldap_set_option($conn, LDAP_OPT_PROTOCOL_VERSION, 3);
-
-            //On construit et sécurise les paramètres de recherche
-            $uid = "uid=".ldap_escape($username, null, LDAP_ESCAPE_FILTER);
-            $ouGroup = "ou=".ldap_escape($group, null, LDAP_ESCAPE_FILTER);
-
-            //On construit le chemin vers le dossier de la classe
-            $dn = $ouGroup.",ou=Etudiants,".$this->getParameter("ldap_base_dn");
-
-            //On accède au dossier
-            $search = @ldap_list($conn, $dn, $uid);
-            if (!$search) {
-                return $this->render('login/students.html.twig', [
-                    'last_username' => $username,
-                    'error' => "Informations de connexion incorrectes !"
-                ]);
-            }
-
-            //On cherche l'utilisateur
-            $user_data = ldap_get_entries($conn, $search);
-            if (count($user_data) == 0) {
+            try {
+                $user_data = $this->ldapFetch(
+                    $username,
+                    $etudiant->getPassword(),
+                    $etudiant->getCodeclasse()->getCodegroupeldap()->getLdapSection()
+                );
+            } catch (AuthenticationException $exception) {
                 return $this->render('inscription/students.html.twig', [
-                    'last_username' => $username,
-                    'error' => "Informations de connexion incorrectes !"
-                ]);
-            }
-            $user_data = $user_data[0]; //On garde que la première entrée
-
-            //On construit le chemin vers l'utilisateur
-            $dn = $uid.",".$dn;
-
-            //On vérifie les informations de connexion de l'utilisateur
-            if (!@ldap_bind($conn, $dn, $etudiant->getPassword())) {
-                return $this->render('inscription/students.html.twig', [
-                    'last_username' => $username,
                     'error' => "Informations de connexion incorrectes !"
                 ]);
             }
@@ -95,19 +62,13 @@ class AuthController extends Controller
             $em->flush();
 
             // Connexion automatique
-            $token = new UsernamePasswordToken($etudiant, null, 'main', $etudiant->getRoles());
-            $this->get('security.token_storage')->setToken($token);
-            $this->get('session')->set('_security_main', serialize($token));
-
-            $event = new InteractiveLoginEvent($request, $token);
-            $this->get("event_dispatcher")->dispatch("security.interactive_login", $event);
-
+            $this->manualLogin($request, $etudiant);
             return $this->redirectToRoute('homepage');
         }
 
         $formview = $form->createView();
 
-        return $this->render("inscription/students.html.twig", [
+        return $this->render('inscription/students.html.twig', [
             'form' => $formview
         ]);
     }
@@ -128,45 +89,16 @@ class AuthController extends Controller
             $username = $professeur->getUsername();
 
             $dbResult = $this->getDoctrine()->getRepository(Professeur::class)
-                ->findOneByUserProf($username);
+                             ->findOneByUserProf($username);
 
             if (!is_null($dbResult)) {
                 $this->get('session')->getFlashBag()->add('error', "Cet utilisateur existe déjà, essayez de vous connecter");
-                return $this->redirectToRoute('connectProf');
+                return $this->redirectToRoute('connectProf', []);
             }
 
-            //Initialisation de la connexion au serveur LDAP
-            $conn  = ldap_connect($this->getParameter('ldap_host'), $this->getParameter('ldap_port'));
-            ldap_set_option($conn, LDAP_OPT_PROTOCOL_VERSION, 3);
-
-            //On construit et sécurise les paramètres de recherche
-            $uid = "uid=".ldap_escape($username, null, LDAP_ESCAPE_FILTER);
-
-            //On construit le chemin vers le dossier de la classe
-            $dn = "ou=Personnel,".$this->getParameter("ldap_base_dn");
-
-            //On accède au dossier
-            $search = @ldap_list($conn, $dn, $uid);
-            if (!$search) {
-                return $this->render('inscription/teachers.html.twig', [
-                    'error' => "Informations de connexion incorrectes !"
-                ]);
-            }
-
-            //On cherche l'utilisateur
-            $user_data = ldap_get_entries($conn, $search);
-            if (count($user_data) == 0) {
-                return $this->render('inscription/teachers.html.twig', [
-                    'error' => "Informations de connexion incorrectes !"
-                ]);
-            }
-            $user_data = $user_data[0]; //On garde que la première entrée
-
-            //On construit le chemin vers l'utilisateur
-            $dn = $uid.",".$dn;
-
-            //On vérifie les informations de connexion de l'utilisateur
-            if (!@ldap_bind($conn, $dn, $professeur->getPassword())) {
+            try {
+                $user_data = $this->ldapFetch($username, $professeur->getPassword());
+            } catch (AuthenticationException $exception) {
                 return $this->render('inscription/teachers.html.twig', [
                     'error' => "Informations de connexion incorrectes !"
                 ]);
