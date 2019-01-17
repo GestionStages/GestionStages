b4b029f41fc490d87c1b502c0a8037f69bcbabc9
Mathieu <mathieu@math98.fr>
2019-01-17 21:07:43 +0100

    Modification des commentaires

--- src/AppBundle/Controller/CommentProfController.php
+++ src/AppBundle/Controller/CommentProfController.php
@@ -86,4 +86,42 @@ class CommentProfController extends Controller
             'form' => $formview
         ]);
     }
+
+    /**
+     * @Route("/prof/commentaires/{id}/edit", name="editProfComment")
+     * @IsGranted("ROLE_PROF")
+     * @param Request $request
+     * @param CommentProf $commentaire
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @throws \Exception
+     */
+    public function editCommentaire(Request $request, CommentProf $commentaire) {
+        $user = $this->getUser();
+
+        if ($user->getId() != $commentaire->getProposition()->getCodeProfesseur()->getId()) {
+            $this->get('session')->getFlashBag()->add('error',"Cette proposition ne vous est pas affectée !");
+            return $this->redirect($this->generateUrl('indexProfPropositions'));
+        }
+
+        $form = $this->createForm(CommentProfType::class, $commentaire);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $commentaire->setDate(new \DateTime('NOW'));
+            $commentaire->setProf($user);
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($commentaire);
+            $em->flush();
+
+            $this->get('session')->getFlashBag()->add('notice','Le commentaire à été modifié !');
+            return $this->redirectToRoute('commentairesProposition', ['id' => $commentaire->getProposition()->getCodeproposition()]);
+        }
+
+        $formview = $form->createView();
+
+        return $this->render('admin/profComments/commentaireAdd.html.twig', [
+            'form' => $formview
+        ]);
+    }
 }
